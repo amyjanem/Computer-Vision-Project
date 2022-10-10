69a9247b531197ba32e2cfbf2de9209dd1c61716
Amy Mallett <amymallett8@gmail.com>
2022-10-10 20:07:45 +0100

    files added

--- manual_rps.py
+++ manual_rps.py
@@ -1,6 +1,4 @@
 # %%
-# %%
-
 
 import random
 
@@ -14,50 +12,54 @@ def get_computer_choice():
 def get_user_choice():
     '''This asks the user for their choise between rock, paper, or scissors, and returns it'''
         
-    user_choice = input("Rock, paper, scissors?\n For Rock: type r, for paper: type p, for scissors: type s")
+    user_choice = input("Rock, paper, or scissors?")
     user_choice.lower()
 
-    if user_choice.isalpha() == False or len(user_choice) != 1:
-        print("Invalid. Please, enter r, p, or s.\n")
-    else:
+    if user_choice in rps_choices:
         return user_choice
-
+    else:
+        print("Invalid. Please type rock, paper, or scissors")
+        get_user_choice()
+    
 
 def get_winner(user_choice, computer_choice):
+    """This compares the users choice and the computers choice, and decides the winner"""
+
     while True:
 
         if user_choice == computer_choice:
-            print(f"The computer chose {computer_choice}. It's a tie!")
+            print(f"You and the computer both chose {computer_choice}. It's a tie!")
             break
 
         elif user_choice == "rock":
             if computer_choice == "paper":
-                print(f"The computer chose {computer_choice}. You lose!")
+                print(f"You chose {user_choice} and the computer chose {computer_choice}.\nYou lose!")
                 break
             else:
-                print(f"The computer chose {computer_choice}. You win!")
+                print(f"You chose {user_choice} and the computer chose {computer_choice}.\nYou win!")
                 break
-
-                                
+                      
         elif user_choice == "paper":
                 if computer_choice == "scissors":
-                    print(f"The computer chose {computer_choice}. You lose!")
+                    print(f"You chose {user_choice} and the computer chose {computer_choice}.\nYou lose!")
                     break
                 else:
-                    print(f"The computer chose {computer_choice}. You win!")
+                    print(f"You chose {user_choice} and the computer chose {computer_choice}.\nYou win!")
+                    break
 
-        else:
+        elif user_choice == "scissors":
             if computer_choice == "rock":
-                print(f"The computer chose {computer_choice}. You lose!")
+                print(f"You chose {user_choice} and the computer chose {computer_choice}.\n You lose!")
                 break
             else:
-                print(f"The computer chose {computer_choice}. You win!")
+                print(f"You chose {user_choice} and the computer chose {computer_choice}.\nYou win!")
+                break
+
+        else:
+            break
                 
-
 def play():
-     
     while True:
-
         computer_choice = get_computer_choice()
         user_choice = get_user_choice()
         result = get_winner(user_choice, computer_choice)
@@ -67,3 +69,4 @@ play()
 
 
 # %%
+
